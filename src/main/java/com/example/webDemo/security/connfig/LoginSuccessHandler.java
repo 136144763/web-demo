2cd7d1699c54d9c19fdb60bc569e7432651df15a
136144763 <luo4545@126.com>
2018-02-13 17:45:07 +0800

    图片验证

--- src/main/java/com/example/webDemo/security/connfig/LoginSuccessHandler.java
+++ src/main/java/com/example/webDemo/security/connfig/LoginSuccessHandler.java
@@ -3,11 +3,9 @@ package com.example.webDemo.security.connfig;
 import com.example.webDemo.security.model.SysUser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
