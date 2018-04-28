90a773ddc7c71de44b92db052082b054ad39bd00
136144763 <luo4545@126.com>
2018-04-28 16:34:12 +0800

    添加aop

--- src/main/java/com/example/webDemo/security/connfig/LogAspect.java
+++ src/main/java/com/example/webDemo/security/connfig/LogAspect.java
@@ -1,9 +1,8 @@
 package com.example.webDemo.security.connfig;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -35,6 +34,38 @@ public class LogAspect {
         System.out.println("ARGS : " + Arrays.toString(joinPoint.getArgs()));
     }
 
+
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+        // 处理完请求，返回内容
+        System.out.println("方法的返回值 : " + ret);
+    }
+
+    //后置异常通知
+    @AfterThrowing("webLog()")
+    public void throwss(JoinPoint jp){
+        System.out.println("方法异常时执行.....");
+    }
+
+    //后置最终通知,final增强，不管是抛出异常或者正常退出都会执行
+    @After("webLog()")
+    public void after(JoinPoint jp){
+        System.out.println("方法最后执行.....");
+    }
+
+    //环绕通知,环绕增强，相当于MethodInterceptor
+    @Around("webLog()")
+    public Object arround(ProceedingJoinPoint pjp) {
+        System.out.println("方法环绕start.....");
+        try {
+            Object o =  pjp.proceed();
+            System.out.println("方法环绕proceed，结果是 :" + o);
+            return o;
+        } catch (Throwable e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 
 }
