7eb925068e0c54dab23a90c996b1415e973db87a
136144763 <luo4545@126.com>
2018-05-09 12:41:18 +0800

    添加swagger接口

--- src/main/java/com/example/webDemo/api/AlipayResource.java
+++ src/main/java/com/example/webDemo/api/AlipayResource.java
@@ -26,4 +26,7 @@ public class AlipayResource {
     public String auth() throws UnsupportedEncodingException {
         return "";
     }
+
+
+
 }
--- src/main/java/com/example/webDemo/security/connfig/WebSecurityConfig.java
+++ src/main/java/com/example/webDemo/security/connfig/WebSecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     final static String[] ignore={
-            "/img/**","/getVerify","/kaptcha","/alipay","/swagger-ui.html"
+            "/img/**","/getVerify","/kaptcha","/alipay","/swagger-ui.html","/webjars/**"
     };
 
     @Autowired
