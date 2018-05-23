ece1f096d52d00745552d44493dab39219495e02
luofei <luo4545@126.com>
2018-05-23 18:04:21 +0800

    添加测试定时器

---
+++ src/main/java/com/example/webDemo/scheduler/TestScheduler.java
@@ -0,0 +1,20 @@
+package com.example.webDemo.scheduler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author luofei on 2018/5/23.
+ */
+@Slf4j
+@Component
+public class TestScheduler {
+
+    @Scheduled
+    public void testScheduler(){
+        
+    }
+
+
+}
