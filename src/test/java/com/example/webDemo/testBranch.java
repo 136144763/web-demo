636cedb71f47e84b3fbf4e2d5882f86bc465c2de
luofei <luo4545@126.com>
2018-05-23 16:06:13 +0800

    branch test

--- src/test/java/com/example/webDemo/testBranch.java
+++ src/test/java/com/example/webDemo/testBranch.java
@@ -1,6 +1,7 @@
 package com.example.webDemo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 
 /**
  * @author luofei on 2018/5/23.
@@ -9,4 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 public class testBranch {
 
 
+    @Test
+    public void testMerge(){
+        log.debug("test");
+    }
 }
