06d600cbaef3528eaefc569c1ff47ee3219e3d9f
luofei <luo4545@126.com>
2018-05-28 14:14:06 +0800

    添加定时器开关

--- src/main/java/com/example/webDemo/scheduler/TestScheduler.java
+++ src/main/java/com/example/webDemo/scheduler/TestScheduler.java
@@ -2,6 +2,7 @@ package com.example.webDemo.scheduler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -12,16 +13,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class TestScheduler {
 
+    @Value("${app.fetch.current.day}")
+    private boolean flag;
+
     /**
      * 第一次调用延时30秒，每秒执行一次
      */
     @Scheduled(initialDelay = 30 * 1000, fixedRate = 1000)
     public void testScheduler() {
-        log.info("current time={}", DateTime.now().toString("yyyy-MM-dd HH:mm:ss"));
+       if(flag){
+           prientCurrentTime();
+       }
     }
 
     @Scheduled(cron = "0 0 3 * * ?")
     public void testSchedulerByCorn() {
+        if (flag) {
+            prientCurrentTime();
+        }
+    }
+
+    public void prientCurrentTime() {
         log.info("current time={}", DateTime.now().toString("yyyy-MM-dd HH:mm:ss"));
     }
 
